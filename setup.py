6110780dd8db4cc019daae90f944a1d5552481d1
Krzysiek <krzysztof.falcman@wp.pl>
2017-09-30 20:32:22 +0200

    Setuptools setup file added

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-scripts-tracker',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='An unofficial Django utility tool that tracks management scripts execution',
+    long_description=README,
+    url='https://krzysiek555.github.io/django-scripts-tracker/',
+    author='Krzysztof Falcman',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ],
+)
